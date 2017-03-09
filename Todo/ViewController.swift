acd6aaf265965cf1109f243832fb103ee72606d9
Jabari Bell <jabari.bell@pxlflu.net>
2017-03-09 16:01:23 -0500

    viewmodel stuff added

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -10,10 +10,34 @@ import UIKit
 import SnapKit
 
 
+struct TodoViewModel {
+    private let datasource = ["Walk dog", "Take out the trash", "Call Mom"]
+    
+    
+    func transformedDatasource() -> [String] {
+        
+        var temp = [String]()
+        
+        for item in datasource {
+            temp.append(item + ":")
+        }
+        
+        return temp
+    }
+}
+
+
 class ViewController: UIViewController {
     
     //MARK: Property
     private let tableView = UITableView(frame: .zero)
+    
+    fileprivate let identifier = "Cell"
+    let viewModel = TodoViewModel()
+    
+    fileprivate lazy var datasource: [String] = {
+        return self.viewModel.transformedDatasource()
+    }()
 
     
     //MARK: Method
@@ -33,12 +57,16 @@ class ViewController: UIViewController {
         
         //frame layout
 //        tableView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
-        tableView.backgroundColor = .purple
+        //good to set background on views to identify
+//        tableView.backgroundColor = .purple
         view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
         
         tableView.snp.makeConstraints { make in
             make.edges.equalTo(view)
         }
+        
+        tableView.dataSource = self
     }
     
     private func setupNetworking() {
@@ -49,5 +77,19 @@ class ViewController: UIViewController {
        //fill in
     }
 
+}
+
+
+//MARK: UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        cell.textLabel?.text = datasource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datasource.count
+    }
 }
 
---
+++ TodoTests/XTests.swift
@@ -0,0 +1,20 @@
+//
+//  XTests.swift
+//  Todo
+//
+//  Created by Jabari Bell on 3/9/17.
+//  Copyright © 2017 ustwo. All rights reserved.
+//
+
+import XCTest
+
+@testable import Todo //DONT FORGET
+
+
+class XTests: XCTestCase {
+    
+    func testThatSomething() {
+        
+    }
+    
+}
