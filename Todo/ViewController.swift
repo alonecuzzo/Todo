c7924ac064d5983e5b5a6d78b1b1983bd2ec9515
Jabari Bell <jabari.bell@pxlflu.net>
2017-03-09 15:31:03 -0500

    tableview on the main view

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import SnapKit
+
 
 class ViewController: UIViewController {
+    
+    //MARK: Property
+    private let tableView = UITableView(frame: .zero)
 
+    
+    //MARK: Method
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setup()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setup() {
+        setupTableView()
+        setupDatabase()
+        setupNetworking()
     }
-
+    
+    
+    private func setupTableView() {
+        
+        //frame layout
+//        tableView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+        tableView.backgroundColor = .purple
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view)
+        }
+    }
+    
+    private func setupNetworking() {
+        //fill in
+    }
+    
+    private func setupDatabase() {
+       //fill in
+    }
 
 }
 
